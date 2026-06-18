e1c4e091781a382572c862aabaf34f5b6b66a14a
COKAIZOKU <proteicode@proton.me>
2026-06-17 22:35:08 -0400

    style: button disabled after clicking

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -310,13 +310,15 @@ function ProductList({ onHomeClick }) {
           {/* Display other plant details like description and cost */}
           <div className="product-description">{plant.description}</div> {/* Display plant description */}
           <div className="product-cost">{plant.cost}</div> {/* Display plant cost */}
-          <button
+            <button
             className="product-button"
-            
-            onClick={() => handleAddToCart(plant)} // Handle adding plant to cart
-          >
-            Add to Cart
-          </button>
+            onClick={() => handleAddToCart(plant)}
+            disabled={addedToCart[plant.name]}
+            >
+            {addedToCart[plant.name]
+                ? "Added to Cart"
+                : "Add to Cart"}
+            </button>
         </div>
       ))}
     </div>
